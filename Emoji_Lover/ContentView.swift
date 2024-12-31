17077ec70bd6a3d10e4a05650a8121090707898c
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-12-31 22:53:40 +0530

    Initial Commit

---
+++ Emoji_Lover/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Emoji_Lover
+//
+//  Created by Thushini Abeysuriya on 2024-02-18.
+//
+
+import SwiftUI
+
+enum Emoji:String , CaseIterable{
+    case 😇 , 😍 , 😝 ,😎
+}
+
+struct ContentView: View {
+    @State var selection:Emoji = .😇
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text(selection.rawValue)
+                    .font(.system(size:150))
+                
+                Picker("Select Emoji",selection: $selection){
+                    ForEach(Emoji.allCases,id:\.self){emoji in
+                        Text(emoji.rawValue)}
+                    
+                }
+                .pickerStyle(.segmented)
+                
+            }
+            .navigationTitle("Emoji Lovers!")
+            .padding()
+        }
+        
+    }
+        
+    
+
+    struct ContentView_Previews:PreviewProvider {
+        static var previews: some View{
+            ContentView()
+        }
+    }
+}
---
+++ Emoji_Lover/Emoji_LoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_LoverApp.swift
+//  Emoji_Lover
+//
+//  Created by Thushini Abeysuriya on 2024-02-18.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
